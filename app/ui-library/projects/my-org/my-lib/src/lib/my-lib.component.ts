47ef7dd4c0af8ce17fb2f02df3376e518e2ed767
Topman Garbuja <topmangarbuja@gmail.com>
2024-12-12 09:38:55 +1100

    create new angular workspace 'ui-library' and create a library '@my-org/my-lib' within

---
+++ app/ui-library/projects/my-org/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p aria-label="title">
+      my-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyLibComponent {
+
+}
